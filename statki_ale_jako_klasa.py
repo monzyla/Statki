501c5f8d2e5eb6df1dc6531d779eac3cdf07e6bc
monzyla <104887073+monzyla@users.noreply.github.com>
2022-06-08 09:05:13 +0200

    Update statki_ale_jako_klasa.py
    Zmienione funkcje dotyczące rozkładania statków na planszy.

--- statki_ale_jako_klasa.py
+++ statki_ale_jako_klasa.py
@@ -7,8 +7,10 @@ class Gra():
         self.kolumny = 10
         self.wymiary = 10
         self.plansza_ukryta = []
+        self.stworz_plansze(self.plansza_ukryta)
         self.plansza_do_gry = []
-        self.statki = [5,4,3,2,1]
+        self.stworz_plansze(self.plansza_do_gry)
+        self.statki = [5,4,4,3,3,3,2,2,2,2,1,1,1,1,1]
 
 
     def stworz_plansze(self, plansza):
@@ -29,36 +31,83 @@ class Gra():
             print()
         print()
 
-    def stworz_statek(self,dlugosc):
+    def rozloz_statki_dla_komputera(self):
+        """Dla każdej długości statku podanego na liście wywołuje funkcje
+        naloz_na_plansze_losowo"""
+        jakie_statki = self.statki
+        plansza = self.plansza_ukryta
+        for statek in jakie_statki:
+            self.naloz_na_plansze_losowo(statek, plansza)
 
-    	while not self.czy_mozna_postawic(wspolrzedne):
-    		orientacja = random.choice(["pionowo", "poziomo"])
-    		if orientacja == "poziomo":
-    		    polozenie_wiersz = random.randint(0, self.wymiary-1)
-    		    polozenie_kolumna = random.randint(0, self.wymiary-dlugosc)
-    		    wspolrzedne = []
-    		    for i in range(dlugosc):
-    		        wspolrzedne.append([polozenie_wiersz, polozenie_kolumna+i])
-    		elif orientacja == "pionowo":
-    		    polozenie_wiersz = random.randint(0, self.wymiary-dlugosc)
-    		    polozenie_kolumna = random.randint(0, self.wymiary-1)
-    		    wspolrzedne = []
-    		    for i in range(dlugosc):
-    		        wspolrzedne.append((polozenie_wiersz+i, polozenie_kolumna))
+    def naloz_na_plansze_losowo(self,statek,plansza):
+        """Dopóki nie da się położyć statku, losuje orientację statku
+        i pozycję jego lewego górnego rogu"""
+        da_sie_polozyc = False
+        orientacja = None
+        wiersz = None
+        kolumna = None
+
+        while da_sie_polozyc == False:
+            orientacja = random.choice(["pionowo","poziomo"])
+            wiersz = random.randint(0,len(plansza))
+            kolumna = random.randint(0,len(plansza[0]))
+            da_sie_polozyc = self.sprawdz(plansza,wiersz,kolumna,orientacja,statek)
+
+        self.poloz_statek(plansza,wiersz,kolumna,orientacja,statek)
 
 
-    def czy_mozna_postawic(self,lista_wspolrzednych):
-        for i in range(len(lista_wspolrzednych)):
-            if self.plansza_ukryta[lista_wspolrzednych[i][0]][lista_wspolrzednych[i][1]] == 1:
+    def generuj(self,wiersz,kolumna,orientacja,dlugosc):
+        """Na podstawie orientacji statku i współrzędnych jego lewego górnego rogu
+        generuje listę wszystkich współrzędnych statku o przekazanej długości"""
+        wspolrzedne = []
+        if orientacja == "poziomo":
+            for i in range(dlugosc):
+                wspolrzedne.append((wiersz, kolumna+i))
+        else:
+            for i in range(dlugosc):
+                wspolrzedne.append((wiersz+i, kolumna))
+        return wspolrzedne
+
+    def wystaje(self,plansza,wiersz,kolumna):
+        """Zwraca True, jeśli (wiersz,kolumna) jest poza planszą"""
+        return wiersz<0 or kolumna<0 or wiersz>= len(plansza) or kolumna>=len(plansza[0])
+
+    def zly_sasiad(self,plansza,wiersz,kolumna):
+        """Zwraca True, jeśli pole nie nadaje się nie sąsiada statku"""
+        if not self.wystaje(plansza,wiersz,kolumna):
+            return plansza[wiersz][kolumna] == 1
+        else:
+            return False
+
+    def sprawdz(self,plansza,wiersz,kolumna,orientacja,statek):
+        """Zwraca True, kiedy żadna ze współrzędnych statku nie znajduje się
+        poza planszą oraz kiedy sąsiadem nie jest pole zajęte przez inny statek"""
+        wspolrzedne_statku = self.generuj(wiersz,kolumna,orientacja,statek)
+        for wiersz_s, kolumna_s in wspolrzedne_statku:
+            if self.wystaje(plansza, wiersz_s, kolumna_s):
                 return False
             else:
-                return True
-        self.postaw_statek(lista_wspolrzednych)
+                if plansza[wiersz_s][kolumna_s]==1:
+                    return False
+                if self.zly_sasiad(plansza, wiersz_s+1,kolumna_s):
+                    return False
+                if self.zly_sasiad(plansza, wiersz_s-1,kolumna_s):
+                    return False
+                if self.zly_sasiad(plansza, wiersz_s,kolumna_s+1):
+                    return False
+                if self.zly_sasiad(plansza, wiersz_s,kolumna_s-1):
+                    return False
+        return True
 
-    def postaw_statek(self, lista):
-        for i in range(len(lista)):
-            self.plansza_ukryta[lista[i][0]][lista[i][1]] = 1
+    def poloz_statek(self,plansza,wiersz,kolumna,orientacja,dlugosc):
+        """Kładzie statek"""
+        gdzie = self.generuj(wiersz,kolumna,orientacja,dlugosc)
+        for wiersz,kolumna in gdzie:
+            plansza[wiersz][kolumna] = 1
 
+    def poloz_uzytkownik(wiersz,kolumna,orientacja,dlugosc):
+        """Do napisania"""
+        pass
 
     def podaj_wiersz(self):
         while True:
@@ -91,10 +140,7 @@ class Gra():
 
 
 moja_gra = Gra()
-moja_gra.stworz_plansze(moja_gra.plansza_ukryta)
-moja_gra.stworz_plansze(moja_gra.plansza_do_gry)
-
-for statek in moja_gra.statki:
-    dlugosc_statku = moja_gra.stworz_statek(statek)
-
+moja_gra.rozloz_statki_dla_komputera()
 moja_gra.wyswietl_plansze(moja_gra.plansza_ukryta)
+
+
